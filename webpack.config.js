c0bf69c0903433adda2992113111b098d426f505
dianannperla <waafirmelyza@hotmail.com>
2025-09-27 13:17:38 +0100

    Update output filename to include timestamp for versioning.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
+    filename: `bundle-${new Date().getTime()}.js`,
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
