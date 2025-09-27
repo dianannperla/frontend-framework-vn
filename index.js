a112a57d53a3fd542d66ae669b2de1632d4ead45
dianannperla <waafirmelyza@hotmail.com>
2025-09-27 13:18:55 +0100

    Update welcome message to include English translation.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Entry point for the application
 console.log('Chào mừng đến với Frontend Framework VN! We love React JS!');
+console.log('Welcome to the Frontend Framework VN! We love React JS!');
