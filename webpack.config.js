8a3399b583340492484904a15c34aa9b7fc0776e
dianannperla <waafirmelyza@hotmail.com>
2025-09-27 13:17:13 +0100

    Update devServer configuration for improved performance

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 };
