4d7f6e2e78ef1f4e06e0d1fdb63292b9be86620f
dianannperla <waafirmelyza@hotmail.com>
2025-09-27 13:17:52 +0100

    Updated output filename format to include a fixed timestamp in the format YYYYMMDD-HHMMSS.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: `bundle-${new Date().getTime()}.js`,
+    filename: `bundle-${new Date().toISOString().slice(0, 19).replace(/T|:/g, '-')}.js`,
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
